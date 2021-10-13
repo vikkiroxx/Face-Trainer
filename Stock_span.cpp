a0cc038c82e200b7a70e84e78811ba866e38da8e
Vikram Singh Chauhan <59952002+vikkiroxx@users.noreply.github.com>
2021-10-13 08:25:49 +0530

    Add files via upload

---
+++ Stock_span.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, arr[100];
+    cin>>n;
+    
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<n; i++){
+        int count = 1;
+        if(i>0){
+            for(int j=0; j<i; j++){
+                if(arr[i]>arr[i-j]){
+                    count++;
+                }
+            }
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
